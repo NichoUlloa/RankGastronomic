ec0a3e9cb5f4e269b3ffb0f2da4efe885c571e3c
Nicolas Aguilar <n.aguilar02@ufrontera.cl>
2023-12-09 23:00:20 -0300

    metodos y clases

--- src/main/java/model/Reseña.java
+++ src/main/java/model/Reseña.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Reseña {
+
+    private String opinion;
+    private int calificacion;
+
+    //constructor
+    public Reseña(String opinion, int calificacion) {
+        this.opinion = opinion;
+        this.calificacion = calificacion;
+    }
+
+    //getters y setters
+
+    public String getOpinion() {
+        return opinion;
+    }
+
+    public void setOpinion(String opinion) {
+        this.opinion = opinion;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    //metodo toString
+    @Override
+    public String toString() {
+        return "Reseña{" + "opinion=" + opinion + ", calificacion=" + calificacion + '}';
+    }
+
+
+
 }
